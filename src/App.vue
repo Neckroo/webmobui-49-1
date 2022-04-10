8fb467ed0b5de1066cd0a1106796a1e36c745b90
Nicolas Chabloz <43348620+Chabloz@users.noreply.github.com>
2022-04-10 10:51:31 +0200

    vite & vue

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+  import { ref, computed, watchEffect } from 'vue';
+
+  const tempSI = ref(0); // kelvin
+
+  const celsius = computed({
+    get: () => tempSI.value - 273.15,
+    set: val => tempSI.value = val + 273.15
+  });
+
+  const kelvin = computed(() => tempSI.value);
+
+  const fahrenheit = computed(() => (tempSI.value - 273.15) * 1.8 + 32);
+
+  watchEffect(() => {
+    console.log('°C', celsius.value);
+    console.log('K', kelvin.value);
+    console.log('°F', fahrenheit.value);
+  });
+
+  setTimeout(() => {
+    tempSI.value = 300;
+  }, 1000)
+
+
+  setTimeout(() => {
+    celsius.value = 40;
+  }, 5000)
+</script>
+
+<template>
+  <h1>Temperatures converter</h1>
+  <div>Celsius: </div>
+  <div>{{ celsius }}</div>
+  <div>Kelvin: </div>
+  <div>{{ kelvin }}</div>
+  <div>{{ fahrenheit }}</div>
+</template>
+
+<style>
+
+</style>
